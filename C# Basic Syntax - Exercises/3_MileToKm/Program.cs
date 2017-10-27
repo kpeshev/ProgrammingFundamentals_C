6ef05b36a169faa4e1aeae7cce317d3b87fae5b1
Koki <kpeshev@yahoo.com>
2017-10-27 20:37:25 +0300

    Add files via upload

---
+++ C# Basic Syntax - Exercises/3_MileToKm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _3_MileToKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal miles = decimal.Parse(Console.ReadLine());
+            decimal km = miles * 1.60934m;
+           
+            Console.WriteLine($"{km:F2}");
+        }
+    }
+}
---
+++ C# Basic Syntax - Exercises/4_BeverageLabels/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4_BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = Console.ReadLine();
+            int Volume = int.Parse(Console.ReadLine());
+			int Energy = int.Parse(Console.ReadLine());
+			int Sugar = int.Parse(Console.ReadLine());
+
+			double kcal = (Volume * Energy) / 100.00;
+			double gram= (Volume * Sugar)/ 100.00;
+
+			Console.WriteLine($"{Volume}ml {Name}:");
+			Console.WriteLine($"{kcal}kcal, {gram}g sugars");
+        }
+    }
+}
